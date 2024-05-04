fea5d69708dc6dd822be4933a1f2c134eac760aa
d4rkeagle6591 <eagle@d4rkeagle.net>
2024-05-04 19:14:52 -0400

    Trying to fix dark mode on prod

--- app/lib/theme/ThemeRegistry.js
+++ app/lib/theme/ThemeRegistry.js
@@ -6,6 +6,8 @@ import NextAppDirEmotionCacheProvider from "@/lib/theme/EmotionCache"
 import { getInitColorSchemeScript } from "@mui/joy/styles"
 import theme from "@/lib/theme/theme"
 
+export const dynamic = 'force-dynamic'
+
 export default function ThemeRegistry({ children }) {
   return (
     <NextAppDirEmotionCacheProvider options={{ key: "joy" }}>

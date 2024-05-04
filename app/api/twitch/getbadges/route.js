cf102c457813dd1113eda1eb248e3aa31474e55a
d4rkeagle6591 <eagle@d4rkeagle.net>
2024-05-04 19:18:03 -0400

    Force dynamic

--- app/api/twitch/getbadges/route.js
+++ app/api/twitch/getbadges/route.js
@@ -1,5 +1,7 @@
 import api from "@/lib/twitch/twitch_api"
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
   const res = await api.get("https://api.twitch.tv/helix/chat/badges/global")
   return new Response(JSON.stringify(res.data.data))

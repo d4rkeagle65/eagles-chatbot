dcbd4bde1ec691416c92c9911ef8f31e129b62f8
d4rkeagle <eagle@d4rkeagle.net>
2024-05-10 18:33:22 -0400

    Completes basic mobile view #3

--- app/page.js
+++ app/page.js
@@ -1,7 +1,4 @@
-import Sheet from "@mui/joy/Sheet"
-import Grid from "@mui/joy/Grid"
 import Box from "@mui/joy/Box"
-import Typography from "@mui/joy/Typography"
 
 export default function Home() {
   return (
